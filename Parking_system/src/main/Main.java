265ed4e24300c8fe8d47eb7de1cbe356251d2577
Andres Mazo <amazobo@eafit.edu.co>
2023-08-01 16:17:45 -0500

    Creating Main
    Creating Main class and first_frame object

---
+++ Parking_system/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import frames.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Background first_frame = new Background();
+		//               Axis x,y width,height
+		first_frame.setBounds(100,30,400,500);
+		first_frame.setVisible(true);
+	}
+}
