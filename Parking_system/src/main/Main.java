b67c8420bc342e58dcc57e124393490350134764
Andres Mazo <amazobo@eafit.edu.co>
2023-08-02 18:06:06 -0500

    Creating labels
    Creating a panel and labels to my frame

--- Parking_system/src/main/Main.java
+++ Parking_system/src/main/Main.java
@@ -5,6 +5,9 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 public class Main {
 	public static void main(String args[]) {
@@ -16,22 +19,36 @@ public class Main {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		loggin.setBounds(634, 278, 201, 103);
+		loggin.setBounds(552, 312, 201, 103);
 		first_frame.getContentPane().add(loggin);
 		
 		JButton btnNewButton = new JButton("Remove a vehicle");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		btnNewButton.setBounds(349, 289, 234, 102);
+		btnNewButton.setBounds(267, 312, 234, 102);
 		first_frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("List of logged vehicles");
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		btnNewButton_1.setBounds(443, 105, 280, 103);
+		btnNewButton_1.setBounds(799, 364, 280, 103);
 		first_frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Exit");
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnNewButton_2.setBounds(540, 425, 226, 103);
 		first_frame.getContentPane().add(btnNewButton_2);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(91, 196, 251));
+		panel.setBounds(0, 10, 1143, 282);
+		first_frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("Java Parking system");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		panel.add(lblNewLabel);
 	}
 }
