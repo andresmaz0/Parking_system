13602d0241a7ed0b417b48a43011102e791561c4
Andres Mazo <amazobo@eafit.edu.co>
2023-08-15 02:13:57 -0500

    Moving buttons and picture
    Trimming menu(buttons, title and picture)

--- Parking_system/src/main/Main.java
+++ Parking_system/src/main/Main.java
@@ -12,40 +12,40 @@ public class Main {
 		
 		Panel panel = new Panel();
 		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(0, 0, 478, 743);
+		panel.setBounds(0, 0, 376, 743);
 		first_frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setBounds(30, 80, 438, 283);
+		lblNewLabel_1.setBounds(-18, 78, 350, 283);
 		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
 		panel.add(lblNewLabel_1);
 		lblNewLabel_1.setIcon(new ImageIcon("D:\\Curso Oracle One\\Parking system\\Pictures\\parking.jpg"));
 		
 		JLabel lblNewLabel = new JLabel("Java Parking system");
-		lblNewLabel.setBounds(85, 10, 322, 44);
+		lblNewLabel.setBounds(30, 24, 322, 44);
 		panel.add(lblNewLabel);
 		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
 		
 		JButton btnNewButton = new JButton("Remove a vehicle");
-		btnNewButton.setBounds(107, 456, 250, 61);
+		btnNewButton.setBounds(61, 460, 250, 61);
 		panel.add(btnNewButton);
 		btnNewButton.setBackground(new Color(255, 255, 255));
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		
 		JButton loggin = new JButton("Log a vehicle");
-		loggin.setBounds(107, 373, 250, 61);
+		loggin.setBounds(61, 371, 250, 61);
 		panel.add(loggin);
 		loggin.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		
 		JButton btnNewButton_1 = new JButton("List of vehicles");
-		btnNewButton_1.setBounds(106, 549, 251, 70);
+		btnNewButton_1.setBounds(60, 549, 251, 70);
 		panel.add(btnNewButton_1);
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		
 		JButton btnNewButton_2 = new JButton("Exit");
-		btnNewButton_2.setBounds(107, 648, 250, 61);
+		btnNewButton_2.setBounds(61, 649, 250, 61);
 		panel.add(btnNewButton_2);
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 22));
 	}
