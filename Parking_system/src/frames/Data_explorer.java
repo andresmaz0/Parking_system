cb784c375a03c5620adb6b002a9c84d928ab0bbe
Andres Mazo <amazobo@eafit.edu.co>
2023-08-15 19:01:27 -0500

    Creating search_button
    new button in the data_explorer panel

--- Parking_system/src/frames/Data_explorer.java
+++ Parking_system/src/frames/Data_explorer.java
@@ -5,9 +5,12 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Data_explorer {
-	private JTable table;
+	private JTable parking_table;
 	
 	public Data_explorer() {
 		Create_panel();
@@ -22,28 +25,13 @@ public class Data_explorer {
 		scrollPane.setBounds(21, 134, 689, 470);
 		panel.add(scrollPane);
 		
-		table = new JTable();
-		table.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		table.setModel(new DefaultTableModel(
+		parking_table = new JTable();
+		parking_table.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		parking_table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
 			},
 			new String[] {
-				"License plate", "Owner", "Entry time", "Departure time", "Payment"
+				"License plate", "Car owner", "Entry time", "Departure time", "Payment"
 			}
 		) {
 			Class[] columnTypes = new Class[] {
@@ -53,12 +41,21 @@ public class Data_explorer {
 				return columnTypes[columnIndex];
 			}
 		});
-		table.getColumnModel().getColumn(0).setPreferredWidth(96);
-		table.getColumnModel().getColumn(1).setPreferredWidth(173);
-		table.getColumnModel().getColumn(2).setPreferredWidth(125);
-		table.getColumnModel().getColumn(3).setPreferredWidth(143);
-		table.getColumnModel().getColumn(4).setPreferredWidth(97);
-		scrollPane.setViewportView(table);
+		parking_table.getColumnModel().getColumn(0).setPreferredWidth(96);
+		parking_table.getColumnModel().getColumn(1).setPreferredWidth(173);
+		parking_table.getColumnModel().getColumn(2).setPreferredWidth(125);
+		parking_table.getColumnModel().getColumn(3).setPreferredWidth(143);
+		parking_table.getColumnModel().getColumn(4).setPreferredWidth(97);
+		scrollPane.setViewportView(parking_table);
+		
+		JButton search_button = new JButton("Search");
+		search_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultTableModel model = (DefaultTableModel)parking_table.getModel();
+			}
+		});
+		search_button.setBounds(315, 645, 85, 21);
+		panel.add(search_button);
 		return panel;
 	}
 }
