86ed437380866ee7a42b254ff2e1ec68b53b11ae
Andres Mazo <amazobo@eafit.edu.co>
2023-08-16 02:32:37 -0500

    Creating class remove_menu

---
+++ Parking_system/src/frames/Remove_menu.java
@@ -0,0 +1,20 @@
+package frames;
+
+import javax.swing.JPanel;
+
+public class Remove_menu {
+	
+	JPanel panel;
+	
+	public Remove_menu() {
+		Create_panel();
+	}
+	
+	public JPanel Create_panel() {
+		panel = new JPanel();
+		panel.setBounds(378, 0, 731, 738);
+		panel.setLayout(null);
+		
+		return panel;
+	}
+}
